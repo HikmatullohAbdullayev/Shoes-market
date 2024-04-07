b552aa4a1d47753ee48027db5f3f2b04453c20e4
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-04-07 20:44:17 +0500

    commit

---
+++ src/assets/icon/YouTubeSvg.jsx
@@ -0,0 +1,22 @@
+const SVGComponent = (props) => (
+  <svg
+    width={13}
+    height={9}
+    viewBox="0 0 13 9"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    {...props}
+  >
+    <desc>{"\n\t\t\tCreated with Pixso.\n\t"}</desc>
+    <defs />
+    <path
+      id="Path"
+      d="M11 -0.13C11.55 0.02 11.98 0.47 12.13 1.03C12.39 2.06 12.39 4.19 12.39 4.19C12.39 4.19 12.39 6.33 12.13 7.36C11.98 7.92 11.55 8.37 11 8.52C10 8.79 6 8.79 6 8.79C6 8.79 1.99 8.79 0.99 8.52C0.44 8.37 0.01 7.92 -0.14 7.36C-0.4 6.33 -0.4 4.19 -0.4 4.19C-0.4 4.19 -0.4 2.06 -0.14 1.03C0.01 0.47 0.44 0.02 0.99 -0.13C1.99 -0.41 6 -0.41 6 -0.41C6 -0.41 10 -0.41 11 -0.13ZM4.8 2.39L4.8 6.39L8 4.4L4.8 2.39Z"
+      fill="#FFFFFF"
+      fillOpacity={1}
+      fillRule="evenodd"
+    />
+  </svg>
+);
+export default SVGComponent;
