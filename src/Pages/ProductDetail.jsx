54f9c1a89e191320ce102022b93e32a0e7e87c48
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-04-16 23:16:21 +0500

    redux add

--- src/Pages/ProductDetail.jsx
+++ src/Pages/ProductDetail.jsx
@@ -1,8 +1,10 @@
-import  {useParams} from "react-router-dom";
+import  {useParams } from "react-router-dom";
 import product from "../data/db.json"
+import "../Pages/style/productDetail.css"
 
 function ProductDetail() {
     const { id } = useParams()
+  
 
         let targetProduct = null;
     
@@ -14,11 +16,31 @@ function ProductDetail() {
     
     console.log(targetProduct);
 
+    
+
     return (
         <section>
             <div className="product">
+                <div className="product_top">
                 <div className="product_img_block">
                     <img className="product_img" src={targetProduct.img} alt={targetProduct.title} />
+                </div>
+                <div className="product_cart">
+                    <div className="product_info_block">
+                    <h2>{targetProduct.title}</h2> 
+                    <p>{targetProduct.info}</p>
+                    <p>{targetProduct.price}</p>
+                    </div>
+                    <div className="product_counter">
+                        <p>Quanity</p>
+                        <div className="counret">
+                            <button>-</button>
+                            <p>1</p>
+                            <button>+</button>
+                        </div>
+                    </div>
+                    <button>Add to Cart</button>
+                </div>
                 </div>
             </div>
             
